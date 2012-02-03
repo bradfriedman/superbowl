681dddf942128b879017e7a76d20ad3b62431088
Brad Friedman <brad.friedman@gmail.com>
2012-02-03 16:06:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superbowl::Application.config.secret_token = 'b0e6a5eb323feeac831abda3605c8624c8704c1a12684070068c38e5b7607d63b3689fd84f708445fccdc4af480dd593a5ffba65bd35165549ae32ca2ffffb75'
